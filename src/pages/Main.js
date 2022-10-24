2c8bf2097cae98127ba1d2c5636cbee646737d39
jyc-coder <jyc4648@gmail.com>
2022-10-24 21:53:15 +0900

    #2 - 메인 페이지 및 유저 검색 컴포넌트 제작
    * App 에서는 `react-router-dom`을 사용해서 상황에 따라 서로 다른 페이지가 나타나게 `Routes`를 작성함
    
    * 메인 페이지에서는 `Main`이라는 컴포넌트가 나타나게 작성하고 내부에는 `SearchInput`컴포넌트를 렌더링하게 설정함
    
    * `SearchInput` :  `Material-ui`를 사용하여 검색창을 제작함. 내용을 입력하고 엔터, 혹은 우측에 있는 `SearchIcon`을 클릭하면 쿼리스트링에 기록되는 방식으로 제작함
    
    * 새로고침을 해도 쿼리스트링의 값을 조회해서 이전에 검색한 값이 유지되도록 설정함

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchInput from '../components/SearchInput/SearchInput'
+
+function Main() {
+  return (
+    <div>
+      <SearchInput/>
+    </div>
+  )
+}
+
+export default Main
